24da379c601af2c8f965b58dccf1ded9bd0c6795
Ruben Trancoso <rubentrancoso@gmail.com>
2017-11-29 09:02:46 -0200

    Reactor implementation

--- accounts-api/src/main/java/com/company/ms/accounts/Application.java
+++ accounts-api/src/main/java/com/company/ms/accounts/Application.java
@@ -1,29 +1,50 @@
 package com.company.ms.accounts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.stereotype.Component;
 
+import com.company.ms.entities.Account;
 import com.company.ms.repositories.AccountRepository;
+import com.company.ms.userapi.message.AccountData;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableJpaRepositories(basePackageClasses = { AccountRepository.class })
+@EnableReactiveMongoRepositories(basePackageClasses = AccountRepository.class)
 @EntityScan(basePackages = { "com.company.ms.entities" })
-@ComponentScan(basePackages = {
-		"com.company.ms.config", 
-		"com.company.ms.audit",
-		"com.company.ms.services", 
-		"com.company.ms.userapi.endpoints"
-	}
-)
+@ComponentScan(basePackages = { "com.company.ms" })
 public class Application {
+
+	@Autowired
+	AccountRepository accountRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Component
+	public class ApplicationRunnerBean implements ApplicationRunner {
+		private final Logger logger = LoggerFactory.getLogger(ApplicationRunnerBean.class);
+
+		@Override
+		public void run(ApplicationArguments strArgs) throws Exception {
+
+			logger.info("Application started...");
+			accountRepository.deleteAll().subscribe();
+			accountRepository.saveAll(Flux.just(new Account(0.0,0.0),new Account(0.0,0.0),new Account(0.0,0.0),new Account(0.0,0.0),new Account(0.0,0.0))).subscribe();		
+			accountRepository.findAll().subscribe(a->System.out.println(((Account)a).toString()));
+		}
 	}
 
 }
--- accounts-api/src/main/java/com/company/ms/userapi/message/AccountData.java
+++ accounts-api/src/main/java/com/company/ms/userapi/message/AccountData.java
@@ -3,32 +3,25 @@ package com.company.ms.userapi.message;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
 @JsonInclude(content=Include.NON_NULL, value=Include.NON_NULL)
 public class AccountData {
 	
-	private Long account_id;
+	private String account_id;
 	private Amount available_credit_limit;
 	private Amount available_withdrawal_limit;
 	
-	public Long getAccount_id() {
-		return account_id;
-	}
-	public void setAccount_id(Long account_id) {
-		this.account_id = account_id;
-	}
-	public Amount getAvailable_credit_limit() {
-		return available_credit_limit;
-	}
-	public void setAvailable_credit_limit(Amount available_credit_limit) {
-		this.available_credit_limit = available_credit_limit;
-	}
-	public Amount getAvailable_withdrawal_limit() {
-		return available_withdrawal_limit;
-	}
-	public void setAvailable_withdrawal_limit(Amount available_withdrawal_limit) {
-		this.available_withdrawal_limit = available_withdrawal_limit;
-	}
-	
-
+	public AccountData(Double  availableCreditLimit, Double availableWithdrawalLimit) {
+		this.available_credit_limit = new Amount(availableCreditLimit);
+		this.available_withdrawal_limit = new Amount(availableWithdrawalLimit);
+	}	
 	
 }
