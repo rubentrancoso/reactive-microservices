da016129a9cb97fb5d26d07f64d82582b7c08ff8
Ruben Trancoso <rubentrancoso@gmail.com>
2017-12-06 17:34:29 -0200

    alien autowired removed

--- accounts-api/src/main/java/com/company/ms/config/CassandraConfigurarion.java
+++ accounts-api/src/main/java/com/company/ms/config/CassandraConfigurarion.java
@@ -1,11 +1,8 @@
 package com.company.ms.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
-
-import com.company.ms.repositories.AccountRepository;
 
 @Configuration
 public class CassandraConfigurarion extends AbstractReactiveCassandraConfiguration {
@@ -15,9 +12,6 @@ public class CassandraConfigurarion extends AbstractReactiveCassandraConfigurati
 
 	@Value("${cassandra.contactpoints}")
 	String contactPoints;
-	
-	@Autowired
-	AccountRepository accountRepository;
 
 	@Override
 	protected String getKeyspaceName() {
